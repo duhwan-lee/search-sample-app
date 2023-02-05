dbab2154b4cc95d5c211b93c7d633da8f055ad2d
doohwanlee <enghks1414@gmail.com>
2023-02-05 23:32:17 +0900

    기본 파일 추가

---
+++ src/main/kotlin/com/dh/sample/searchsampleapp/infrastructure/SearchSampleAppApplication.kt
@@ -0,0 +1,11 @@
+package com.dh.sample.searchsampleapp.infrastructure
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SearchSampleAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<SearchSampleAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/dh/sample/searchsampleapp/SearchSampleAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dh.sample.searchsampleapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SearchSampleAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
